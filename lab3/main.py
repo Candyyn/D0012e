0fc817b361f7e2cea668eed64f60bf68fdc049d8
Candyyn <Candyynnz@gmail.com>
2021-12-10 14:42:41 +0100

    tree

--- lab3/main.py
+++ lab3/main.py
@@ -4,8 +4,58 @@ class node:
         self.left = None
         self.right = None
 
+    def display(self):
+        lines, *_ = self._display_aux()
+        for line in lines:
+            print(line)
 
-def insert(root, data): #O(n)
+    def _display_aux(self):
+        """Returns list of strings, width, height, and horizontal coordinate of the root."""
+        # No child.
+        if self.right is None and self.left is None:
+            line = '%s' % self.data
+            width = len(line)
+            height = 1
+            middle = width // 2
+            return [line], width, height, middle
+
+        # Only left child.
+        if self.right is None:
+            lines, n, p, x = self.left._display_aux()
+            s = '%s' % self.data
+            u = len(s)
+            first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s
+            second_line = x * ' ' + '/' + (n - x - 1 + u) * ' '
+            shifted_lines = [line + u * ' ' for line in lines]
+            return [first_line, second_line] + shifted_lines, n + u, p + 2, n + u // 2
+
+        # Only right child.
+        if self.left is None:
+            lines, n, p, x = self.right._display_aux()
+            s = '%s' % self.data
+            u = len(s)
+            first_line = s + x * '_' + (n - x) * ' '
+            second_line = (u + x) * ' ' + '\\' + (n - x - 1) * ' '
+            shifted_lines = [u * ' ' + line for line in lines]
+            return [first_line, second_line] + shifted_lines, n + u, p + 2, u // 2
+
+        # Two children.
+        left, n, p, x = self.left._display_aux()
+        right, m, q, y = self.right._display_aux()
+        s = '%s' % self.data
+        u = len(s)
+        first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s + y * '_' + (m - y) * ' '
+        second_line = x * ' ' + '/' + (n - x - 1 + u + y) * ' ' + '\\' + (m - y - 1) * ' '
+        if p < q:
+            left += [n * ' '] * (q - p)
+        elif q < p:
+            right += [m * ' '] * (p - q)
+        zipped_lines = zip(left, right)
+        lines = [first_line, second_line] + [a + u * ' ' + b for a, b in zipped_lines]
+        return lines, n + m + u, max(p, q) + 2, n + u // 2
+
+
+def insert(root, data):  # O(n)
     if root is None:
         root = node(data)
     else:
@@ -18,10 +68,11 @@ def insert(root, data): #O(n)
 
 def displayAll(root):
     if root:
+        print('left ')
         displayAll(root.left)
         print(root.data)
+        print('right ')
         displayAll(root.right)
-
 
 
 if __name__ == '__main__':
@@ -34,4 +85,4 @@ if __name__ == '__main__':
     test = insert(test, 15)
     test = insert(test, 1)
 
-    displayAll(test)
+    test.display()
